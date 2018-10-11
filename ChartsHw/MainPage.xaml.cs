48f7e71e52b8995054139484101c3f0df2a1d2bc
Pall <gudla001@cougars.csusm.edu>
2018-10-10 19:11:07 -0700

    * .gitignore: Homework 6 finished - Microcharts
    * ChartsHw.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * ChartsHw.csproj:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * ChartsHw.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * ChartsHw.Android.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Tabbar.axml:
    * LaunchScreen.storyboard:
    * Toolbar.axml:
    * Default-Portrait@2x.png:
    * Icon.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * Icon180.png:
    * Icon120.png:
    * Icon167.png:
    * Icon152.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ ChartsHw/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microcharts;
+using SkiaSharp;
+using Xamarin.Forms;
+using Entry = Microcharts.Entry;
+
+
+namespace ChartsHw
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            CreateCharts();
+            MainPicker.Items.Add("BarChart");
+            MainPicker.Items.Add("DonutChart");
+            MainPicker.Items.Add("RadialGaugeChart");
+            Chart1.IsVisible = !Chart1.IsVisible;
+            Chart2.IsVisible = !Chart2.IsVisible;
+            Chart3.IsVisible = !Chart3.IsVisible;
+
+        }
+
+        void Handle_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            var name = MainPicker.Items[MainPicker.SelectedIndex];
+            if(name == "BarChart")
+            {
+                Chart1.IsVisible = true;
+                Chart2.IsVisible = false;
+                Chart3.IsVisible = false;
+            }
+            else if (name == "DonutChart")
+            {
+                Chart1.IsVisible = false;
+                Chart2.IsVisible = true;
+                Chart3.IsVisible = false;
+            }
+            else if (name == "RadialGaugeChart")
+            {
+                Chart1.IsVisible = false;
+                Chart2.IsVisible = false;
+                Chart3.IsVisible = true;
+            }
+
+        }
+        private void CreateCharts()
+        {
+            var entryList = new List<Entry>()
+            {
+                 new Entry(100)
+                {
+                    Label = "Goes to College",
+                    Color = SKColor.Parse("#FF2918"),
+                },
+                new Entry(36)
+                {
+                    Label = "Gets Bachelors degree in 4 years",
+                    Color = SKColor.Parse("#2FF234"),
+                },
+                new Entry(70)
+                {
+                    Label = "Graduate from college",
+                    Color = SKColor.Parse("#23CCC2"),
+
+                }
+            };
+            var barChart = new BarChart() { Entries = entryList };
+            var donutChart = new DonutChart() { Entries = entryList };
+            var someChart = new RadialGaugeChart() { Entries = entryList };
+            Chart1.Chart = barChart;
+            Chart2.Chart = donutChart;
+            Chart3.Chart = someChart;
+        }
+
+
+
+    }
+}
